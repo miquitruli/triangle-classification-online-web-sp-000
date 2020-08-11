cf157283ba081342970ae811ca5c9bc89b989d9a
MICAELLA COLOMBO <micaelc@g.clemson.edu>
2020-08-11 22:42:51 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,18 @@
 class Triangle
-  # write code here
+  attr_accessor :length_A, :length_B, :length_C
+  @triangle = []
+
+  def initialize(length_A, length_B, length_C)
+    @length_A = length_A
+    @length_B = length_B
+    @length_C = length_C
+    @triangle = length_A, length_B, length_C
+  end
+
+  def equilateral? #all sides are equal
+    (length_A == length_B) && (length_A == length_C)
+  end
+
+  class TriangleError < StandardError
+  end
 end
